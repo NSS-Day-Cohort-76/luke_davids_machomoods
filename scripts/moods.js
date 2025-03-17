4818ecef4d37c0169aefff5af5c5e81791104937
Luke Davids <lukedavids@gmail.com>
2025-03-17 16:00:51 -0500

    applied initial CSS styling

--- scripts/moods.js
+++ scripts/moods.js
@@ -4,8 +4,8 @@ export const makeMoods = () => {
     let HTMLString = ""
     for (const item of database) {
         HTMLString += `
-        <section class="mood">${item.name}
-        <img class="moodImg" alt="${item.text} image" src="${item.imageURL}">
+        <section class="mood"><h2>${item.name}</h2>
+        <img class="moodImg" alt="${item.text} image" src="${item.imageUrl}">
             <div>
                 <h3 class="machoAdvice">Macho Advice:</h3>
                 <ul>
@@ -17,5 +17,4 @@ export const makeMoods = () => {
         
     }
     return HTMLString
-
 }
